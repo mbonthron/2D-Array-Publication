f08d1df357a3b863d424ece33b452dfa839f5d49
Terrence Pierce <65622821+TerrenceCPierce@users.noreply.github.com>
2025-08-10 22:06:02 -0400

    Run 8_8_2025

--- General Optimization/main_optimization_structural.m
+++ General Optimization/main_optimization_structural.m
@@ -167,97 +167,97 @@ end
 
 
 %% Run Optimization
-% myCluster = parcluster('Processes');
-% delete(myCluster.Jobs);
-% myCluster.NumWorkers = 4;
-% saveProfile(myCluster);
-% OG_data = data;
-% for count=0:(2^num_arches-1)
-%     if results{count+2,stable_idx} && results{count+2,asym_idx} && ~results{count+2,prohib_idx} && results{count+2,connected_idx}
-%         raw_data = init_shape_structural(shapeNum, OG_data, count);
-% 
-%         % Find arches with negative area to force positive
-%         raw_data.arches_to_force_positive = find(results{count+2,archd_idx} < -.3)';
-%         % Find arches with negative area to force positive
-%         raw_data.arches_to_force_negative = find(results{count+2,archd_idx} > .3)';
-% 
-%         for t_idx = 1:length(tvals)
-%             t = tvals(t_idx);
-%             % Run COCO
-%             data = raw_data;
-%             data.t = t;
-%             data.t_vector = t*ones(data.N,1);
-%             bpoints = OG_bpoints;
-%             %% Run Optimization
-%             if ~exist("TransitionExcel\"+ data.timeStr + "\", 'dir')
-%                 mkdir("TransitionExcel\"+data.timeStr + "\");
-%             end
-%             trans_percent_tensor(:,:,t_idx) = optimize(data, bpoints, betavals, sigmavals);
-%             close all hidden;
-%             % if tensor_true % Check for errors if bpoints change size in COCO
-%             %     if length(bpoints) == length(OG_bpoints)
-%             %         trans_percent_tensor(:,:,t_idx) = optimize(data, bpoints, betavals);
-%             %         save("temp", "trans_percent_tensor");
-%             %     else
-%             %         if any(trans_percent_tensor) % Check if data has been written
-%             %             for i=1:(t_idx-1)
-%             %                 trans_percent_cell{i,1} = trans_percent_tensor(:,:,i);
-%             %                 trans_percent_cell{i,2} = OG_bpoints;
-%             %             end
-%             %         end
-%             %         trans_percent_cell{t_idx,1} = optimize(data, bpoints, betavals);
-%             %         trans_percent_cell{t_idx,2} = bpoints;
-%             %         tensor_true = 0;
-%             %     end
-%             % else
-%             %     trans_percent_cell{t_idx,1} = optimize(data, bpoints, betavals);
-%             %     trans_percent_cell{t_idx,2} = bpoints;
-%             %     save("temp", "trans_percent_cell");
-%             % end
-%         end
-% 
-%         data = raw_data;
-%         bpoints = OG_bpoints;
-% 
-%         if tensor_true
-%             for beta_idx = 1:length(betavals)
-%                 beta = betavals(beta_idx);
-%                 results_cell = [ {'b/t'}, num2cell(tvals/pi); ...
-%                     num2cell(bpoints/pi)', squeeze(num2cell(trans_percent_tensor(:,beta_idx,:)))];
-% 
-%                 if ~exist("TransitionExcel\"+ data.timeStr + "\", 'dir')
-%                     mkdir("TransitionExcel\"+data.timeStr + "\");
-%                 end
-%                 data.file_name_trans = data.timeStr + "\"+ data.shape_name + " beta = " + num2str(beta) + " NumCells = "+ num2str(data.N_cells);
-% 
-% 
-%                 writecell(results_cell, "TransitionExcel\"+data.file_name_trans +".xlsx");
-% 
-%                 figure(77); clf; hold on;
-%                 title("Transition Percent " + data.shape_name + " beta = " + num2str(beta) + " NumCells = "+ num2str(data.N_cells));
-%                 xlabel("t");
-%                 ylabel("b");
-%                 clim([0 100]);
-%                 a=colorbar;
-%                 a.Label.String = 'Transition Percent %';
-%                 imagesc(tvals/pi, bpoints/pi, squeeze(trans_percent_tensor(:,beta_idx,:)))
-%                 print(gcf, "TransitionExcel\"+data.file_name_trans + " - Visual.png", '-dpng', '-r600');
-%             end
-%         else
-%             for t_idx = 1:length(tvals)
-%                 t = tvals(t_idx);
-%                 bvals = trans_percent_cell{t_idx,2};
-%                 results_cell = [ {'b/beta'}, num2cell(betavals); ...
-%                     num2cell(bvals/pi)', squeeze(num2cell(trans_percent_cell{t_idx,1}))];
-% 
-%                 if ~exist("TransitionExcel\"+ data.timeStr + "\", 'dir')
-%                     mkdir("TransitionExcel\"+data.timeStr + "\");
-%                 end
-%                 data.file_name_trans = data.timeStr + "\"+ data.shape_name + " t = " + num2str(t) + " NumCells = "+ num2str(data.N_cells);
-% 
-% 
-%                 writecell(results_cell, "TransitionExcel\"+data.file_name_trans +".xlsx");
-%             end
-%         end
-%     end
-% end
+myCluster = parcluster('Processes');
+delete(myCluster.Jobs);
+myCluster.NumWorkers = 4;
+saveProfile(myCluster);
+OG_data = data;
+for count=0:(2^num_arches-1)
+    if ~any(cellfun(@isempty, results(count+2,:))) && results{count+2,stable_idx} && results{count+2,asym_idx} && ~results{count+2,prohib_idx} && results{count+2,connected_idx}
+        raw_data = init_shape_structural(shapeNum, OG_data, count);
+
+        % Find arches with negative area to force positive
+        raw_data.arches_to_force_positive = find(results{count+2,archd_idx} < -.3)';
+        % Find arches with negative area to force positive
+        raw_data.arches_to_force_negative = find(results{count+2,archd_idx} > .3)';
+
+        for t_idx = 1:length(tvals)
+            t = tvals(t_idx);
+            % Run COCO
+            data = raw_data;
+            data.t = t;
+            data.t_vector = t*ones(data.N,1);
+            bpoints = OG_bpoints;
+            %% Run Optimization
+            if ~exist("TransitionExcel\"+ data.timeStr + "\", 'dir')
+                mkdir("TransitionExcel\"+data.timeStr + "\");
+            end
+            trans_percent_tensor(:,:,t_idx) = optimize(data, bpoints, betavals, sigmavals);
+            close all hidden;
+            % if tensor_true % Check for errors if bpoints change size in COCO
+            %     if length(bpoints) == length(OG_bpoints)
+            %         trans_percent_tensor(:,:,t_idx) = optimize(data, bpoints, betavals);
+            %         save("temp", "trans_percent_tensor");
+            %     else
+            %         if any(trans_percent_tensor) % Check if data has been written
+            %             for i=1:(t_idx-1)
+            %                 trans_percent_cell{i,1} = trans_percent_tensor(:,:,i);
+            %                 trans_percent_cell{i,2} = OG_bpoints;
+            %             end
+            %         end
+            %         trans_percent_cell{t_idx,1} = optimize(data, bpoints, betavals);
+            %         trans_percent_cell{t_idx,2} = bpoints;
+            %         tensor_true = 0;
+            %     end
+            % else
+            %     trans_percent_cell{t_idx,1} = optimize(data, bpoints, betavals);
+            %     trans_percent_cell{t_idx,2} = bpoints;
+            %     save("temp", "trans_percent_cell");
+            % end
+        end
+
+        data = raw_data;
+        bpoints = OG_bpoints;
+
+        if tensor_true
+            for beta_idx = 1:length(betavals)
+                beta = betavals(beta_idx);
+                results_cell = [ {'b/t'}, num2cell(tvals/pi); ...
+                    num2cell(bpoints/pi)', squeeze(num2cell(trans_percent_tensor(:,beta_idx,:)))];
+
+                if ~exist("TransitionExcel\"+ data.timeStr + "\", 'dir')
+                    mkdir("TransitionExcel\"+data.timeStr + "\");
+                end
+                data.file_name_trans = data.timeStr + "\"+ data.shape_name + " beta = " + num2str(beta) + " NumCells = "+ num2str(data.N_cells);
+
+
+                writecell(results_cell, "TransitionExcel\"+data.file_name_trans +".xlsx");
+
+                figure(77); clf; hold on;
+                title("Transition Percent " + data.shape_name + " beta = " + num2str(beta) + " NumCells = "+ num2str(data.N_cells));
+                xlabel("t");
+                ylabel("b");
+                clim([0 100]);
+                a=colorbar;
+                a.Label.String = 'Transition Percent %';
+                imagesc(tvals/pi, bpoints/pi, squeeze(trans_percent_tensor(:,beta_idx,:)))
+                print(gcf, "TransitionExcel\"+data.file_name_trans + " - Visual.png", '-dpng', '-r600');
+            end
+        else
+            for t_idx = 1:length(tvals)
+                t = tvals(t_idx);
+                bvals = trans_percent_cell{t_idx,2};
+                results_cell = [ {'b/beta'}, num2cell(betavals); ...
+                    num2cell(bvals/pi)', squeeze(num2cell(trans_percent_cell{t_idx,1}))];
+
+                if ~exist("TransitionExcel\"+ data.timeStr + "\", 'dir')
+                    mkdir("TransitionExcel\"+data.timeStr + "\");
+                end
+                data.file_name_trans = data.timeStr + "\"+ data.shape_name + " t = " + num2str(t) + " NumCells = "+ num2str(data.N_cells);
+
+
+                writecell(results_cell, "TransitionExcel\"+data.file_name_trans +".xlsx");
+            end
+        end
+    end
+end
